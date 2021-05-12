b786065117c842d3ddd0b53fb5b8f058cac77a39
Camal Cakar <camal.cakar@jimdo.com>
2021-05-12 11:09:49 +0200

    test(item): Add boundary test for items backstage, sulfuras

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -40,4 +40,85 @@ internal class GildedRoseTest {
         assertEquals(-2, app.items[0].sellIn)
         assertEquals(0, app.items[0].quality)
     }
+
+    @Test
+    fun agedBrieIncreaseInQuality() {
+        val items = arrayOf(Item("Aged Brie", 1, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].sellIn)
+        assertEquals(2, app.items[0].quality)
+    }
+
+    @Test
+    fun agedBrieDoubleInQuality() {
+        val items = arrayOf(Item("Aged Brie", 0, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(-1, app.items[0].sellIn)
+        assertEquals(3, app.items[0].quality)
+    }
+
+    @Test
+    fun agedBrieNeverPassesInQualityOver50() {
+        val items = arrayOf(Item("Aged Brie", 0, 50))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(-1, app.items[0].sellIn)
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun sulfurasNeverChanges() {
+        val items = arrayOf(Item("Sulfuras, Hand of Ragnaros", 1, 80))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(1, app.items[0].sellIn)
+        assertEquals(80, app.items[0].quality)
+    }
+
+    @Test
+    fun backstageIncreaseInQualitySellInGreate10() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 11, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(10, app.items[0].sellIn)
+        assertEquals(2, app.items[0].quality)
+    }
+
+    @Test
+    fun backstageIncreasesBy2QualitySellIn10() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(9, app.items[0].sellIn)
+        assertEquals(3, app.items[0].quality)
+    }
+
+    @Test
+    fun backstageIncreasesBy2QualitySellIn6() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 6, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(5, app.items[0].sellIn)
+        assertEquals(3, app.items[0].quality)
+    }
+
+    @Test
+    fun backstageIncreasesBy2QualitySellIn5() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(4, app.items[0].sellIn)
+        assertEquals(4, app.items[0].quality)
+    }
+
+    @Test
+    fun backstageWorthlessQualitySellIn0() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(-1, app.items[0].sellIn)
+        assertEquals(0, app.items[0].quality)
+    }
 }
